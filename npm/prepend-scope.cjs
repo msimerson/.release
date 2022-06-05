20e8e5dbcf634c2f568d973966be42c4504db480
Matt Simerson <matt@tnpi.net>
2022-06-05 13:08:05 -0700

    rename prepend-scope.js -> .csj

--- npm/prepend-scope.cjs
+++ npm/prepend-scope.cjs
@@ -1,13 +1,13 @@
 #!node
+const fs = require('fs')
 function usage () {
     console.log(`${process.argv[1]} <scope>`)
     process.exit(1)
 }
 const scope = process.argv[2]; if (!scope) usage()
-const fs = require('fs')
 const pkg = JSON.parse(fs.readFileSync('./package.json'))
 const checkExistsRe = new RegExp(`^${scope}/`, 'g')
-// console.log(`name: ${pkg.name}, scope: ${scope}, re: ${checkExistsRe}`)
+console.log(`name: ${pkg.name}, scope: ${scope}, re: ${checkExistsRe}`)
 if (checkExistsRe.test(pkg.name)) {
     console.error(`already scoped to ${scope}`)
     process.exit(1)
