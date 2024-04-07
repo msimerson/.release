be0e87a195cd24d03a5440ac7c39419f36e29bbc
Matt Simerson <matt@tnpi.net>
2024-04-07 12:09:29 -0700

    finish: if PR is merged, delete the remote branch

--- finish.sh
+++ finish.sh
@@ -6,6 +6,7 @@ CURRENT_BRANCH=$(git branch --show-current)
 
 get_main_branch
 
+# publish the the draft release
 if command -v gh; then
     PKG_VERSION=$(node -e 'console.log(require("./package.json").version)')
     gh release edit "v$PKG_VERSION" --draft=false
@@ -13,8 +14,11 @@ fi
 
 if [ "$CURRENT_BRANCH" != "$MAIN_BRANCH" ];
 then
-    # TODO, maybe: delete remote branch
-    # git push origin ":$CURRENT_BRANCH"
+    # if the PR is merged, delete the remote branch
+    _state=$(gh pr view "$CURRENT_BRANCH" | grep -i state | awk '{ print $2 }')
+    if [ "$_state" = "MERGED" ]; then
+        git push origin ":$CURRENT_BRANCH"
+    fi
 
     git checkout "$MAIN_BRANCH"
     git branch -d "$CURRENT_BRANCH"
