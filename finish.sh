e5ca16937e4a27d394544100e39e2d1fb532e77e
Matt Simerson <matt@tnpi.net>
2025-06-16 09:46:03 -0700

    finish: add some status messages

--- finish.sh
+++ finish.sh
@@ -12,15 +12,16 @@ get_main_branch
 
 delete_remote_branch()
 {
+    echo "deleting the remote branch"
     git push origin ":$CURRENT_BRANCH"
     BRANCH_DEL="-D"
 }
 
 edit_release_body()
 {
-    # update the release body to the contents of the PR body
     PR_BODY="$(gh pr view --json body --jq .body)"
     if [ -n "$PR_BODY" ]; then
+        echo "Setting release body to the PR body contents"
         gh release edit "v$PKG_VERSION" --notes "$PR_BODY"
     fi
 }
@@ -47,8 +48,8 @@ if command -v gh; then
         exit 1
     fi
 
-    # publish the draft release
     if [ "$REL_IS_DRAFT" = "true" ]; then
+        echo "Publishing the v$PKG_VERSION to GH release"
         gh release edit "v$PKG_VERSION" --draft=false
         update_major_version_tag
     fi
@@ -56,7 +57,6 @@ fi
 
 if [ "$CURRENT_BRANCH" != "$MAIN_BRANCH" ];
 then
-    # if the PR is merged, delete the remote branch
     if [ "$PR_STATE" = "MERGED" ]; then
         delete_remote_branch
         edit_release_body
