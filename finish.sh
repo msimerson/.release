6c40162fcc5f5ef6a2fedaf92712384e066734e3
Matt Simerson <matt@tnpi.net>
2026-01-05 12:19:29 -0800

    finish: warn (was fatal) when no draft release

--- finish.sh
+++ finish.sh
@@ -41,7 +41,8 @@ update_major_version_tag()
 
 if command -v gh; then
     PR_STATE=$(gh pr view "$CURRENT_BRANCH" | grep -i state | awk '{ print $2 }')
-    REL_IS_DRAFT=$(gh release view "v$PKG_VERSION" --json isDraft --jq '.isDraft')
+    REL_IS_DRAFT=$(gh release view "v$PKG_VERSION" --json isDraft --jq '.isDraft') \
+        || echo "no draft release for $PKG_VERSION"
 
     if [ "$PR_STATE" != "MERGED" ]; then
         echo "WARNING: PR not merged, bailing out"
